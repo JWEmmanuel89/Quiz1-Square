e8ed3df4e0130beca561cc41801ef1ffeb314265
JWEmmanuel89 <90356423+JWEmmanuel89@users.noreply.github.com>
2023-01-29 15:34:12 -0600

    Final and test

--- main.go
+++ main.go
@@ -2,9 +2,19 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 // #9. Create function Square, will return area and perimter
 func square(side float64) (float64, float64) {
 	area := side * side
 	perimeter := 4 * side
 	return area, perimeter
+}
+
+func main() {
+	// #10. Test of square function
+	result1, result2 := square(5.0)
+	fmt.Println(result1, result2)
 }
